f802ae50a5ded190c41d68cf49594a8672e6db8f
Caroline Fallesen <carolinefallesen@gmail.com>
2024-01-03 19:31:23 +0100

    Added server and client
    * Server is created same way as in single task. All unnecessary data and classes have been removed
    * Client is created from Cucumber Example. All unnecessary classes have been removed.
    * Added all scenarios and steps from task.
    * Created class for SimpleDTUPay

---
+++ Simple DTU Pay Client/src/test/java/dtu/example/SimpleDTUPay.java
@@ -0,0 +1,11 @@
+package dtu.example;
+
+public class SimpleDTUPay {
+
+    public SimpleDTUPay() {
+
+    }
+    public boolean pay(int amount, String cid, String mid) {
+        return false;
+    }
+}
