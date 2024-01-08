b1d9367a2bb664004eb25cc4f049f6ee32233d70
MarianCCulea <marianclaudiu.culea@gmail.com>
2024-01-08 15:40:20 +0100

    Docker setup

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,2 +1,19 @@
-cd Simple\ DTU\ Pay\ Server/
-./mvnw test
+pushd Simple\ DTU\ Pay\ Server
+./mvnw package
+# Create a new docker image if necessary.
+docker-compose build
+# Restarts the container with the new image if necessary
+docker-compose up -d
+# The server stays running.
+# To terminate the server run docker-compose down in the
+# code-with-quarkus direcgtory
+# clean up images
+docker image prune -f
+popd
+
+# Give the Web server a chance to finish start up
+sleep 2
+
+pushd Simple\ DTU\ Pay\ Client
+mvn test
+popd
